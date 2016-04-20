ffe17f1deae2fe4933cd8b943fa0a475daa6111e
Mitchell Gordon <mgordon@uber.com>
2016-04-19 17:51:32 -0700

    Mocking Mutex interface

---
+++ mocks/Mutex.go
@@ -0,0 +1,49 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+type Mutex struct {
+	mock.Mock
+}
+
+// Lock provides a mock function with given fields:
+func (_m *Mutex) Lock() error {
+	ret := _m.Called()
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func() error); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// Unlock provides a mock function with given fields:
+func (_m *Mutex) Unlock() bool {
+	ret := _m.Called()
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func() bool); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	return r0
+}
+
+// Extend provides a mock function with given fields:
+func (_m *Mutex) Extend() bool {
+	ret := _m.Called()
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func() bool); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	return r0
+}
